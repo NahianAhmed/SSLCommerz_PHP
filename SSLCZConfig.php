aff877cea7e23ad4b68bb4c378c5d8442f259e5d
Nahian Ahmed <nahianahmedcse@gmail.com>
2019-03-20 15:46:39 +0600

    1st time

---
+++ SSLCZConfig.php
@@ -0,0 +1,13 @@
+<?php
+
+// define("SSLCZ_STORE_ID", "demotest");
+// define("SSLCZ_STORE_PASSWD", "qwerty");
+
+define("SSLCZ_STORE_ID", "bugfr5c91dfee216c4");
+define("SSLCZ_STORE_PASSWD", "bugfr5c91dfee216c4@ssl");
+
+# IF SANDBOX TRUE, THEN IT WILL CONNECT WITH SSLCOMMERZ SANDBOX (TEST) SYSTEM
+define("SSLCZ_IS_SANDBOX", true);
+
+# IF BROWSE FROM LOCAL HOST, KEEP true
+define("SSLCZ_IS_LOCAL_HOST", true);
